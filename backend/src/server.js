a7a454d276fc57ff9fe49fd0aa724cfbf12ca5db
Vishnu Datta <vishnudatta2004@gmail.com>
2025-11-03 23:58:43 +0530

    Fixed Rate Limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from 'cors'
 
 import {connectDB} from "./config/db.js"
 import rateLimiter from "./middleware/ratelimiter.js"
@@ -14,8 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 
 // middleware
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"]
+}));
 app.use(express.json());
 app.use(rateLimiter);
+
 
 //routes
 app.use("/api/notes" , noteRoutes);
