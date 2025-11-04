c3b06495e3d4acca5b3b600b3b309326c25aeed7
Vishnu Datta <vishnudatta2004@gmail.com>
2025-11-05 00:14:08 +0530

    Changed the Cors and axios for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from 'cors'
+import path from "path";
 
 import {connectDB} from "./config/db.js"
 import rateLimiter from "./middleware/ratelimiter.js"
@@ -12,12 +13,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
 // middleware
-app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"]
-}));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"]
+    }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -25,8 +29,15 @@ app.use(rateLimiter);
 //routes
 app.use("/api/notes" , noteRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")))
+    app.get("/" , (req , res) => {
+    res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"));
+})
+}
+
 
 connectDB().then(()=> {
-app.listen(5000 , () => {
+app.listen(PORT , () => {
     console.log(`Server Running on port ${PORT}`);
 })});
