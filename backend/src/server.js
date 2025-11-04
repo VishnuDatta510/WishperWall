cb3127d5e9b7825082d09f94f9401298067a13a4
Vishnu Datta <vishnudatta2004@gmail.com>
2025-11-05 01:55:38 +0530

    fixed bugs

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // middleware
@@ -29,7 +29,7 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
